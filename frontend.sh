fb62bc642d89a5e1d70b8f273ef68f09780e1420
Prabhu <prabh.rao@gmail.com>
2023-02-19 22:36:08 +0530

    in out log and function

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ log_file=/tmp/roboshop.log
 rm -f ${log_file}
 
 print_head() {
-  echo -e "\e[35m$1\e[0m"
+  echo -e "\e[36m$1\e[0m"
 }
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
@@ -11,7 +11,7 @@ yum install nginx -y &>>${log_file}
 print_head "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-print_head"Downloading Frontend Content"
+print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
@@ -20,7 +20,7 @@ print_head "\Copying Nginx config for Roboshop"
 # cp ${code_dir}/configs/nginx.roboshop.conf etc/nginx/default.d/roboshop.conf
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-print_head"Enabling Nginx"
+print_head "Enabling Nginx"
 systemctl enable nginx &>>${log_file}
 
 echo -e "Restart Nginx"
